89b8ed28883b19eafeafa2f0ad0e045199498e17
ajallen14 <ajallen142001@gmail.com>
2025-08-02 11:49:36 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:useless/splash_screen.dart';
+
+void main() {
+  runApp(const UnsolvablePuzzleApp());
+}
+
+class UnsolvablePuzzleApp extends StatelessWidget {
+  const UnsolvablePuzzleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'The Unsolvable Puzzle',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'Inter',
+      ),
+      // The app now starts with the splash screen
+      home: const SplashScreen(),
+    );
+  }
+}
